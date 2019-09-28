e9f4680c128c49c952b9976fd7baa9e5eee765cc
DaniloJesus <danilo@almejus.com>
2019-09-28 03:15:23 -0300

    Criando o tema NanaNoPote

---
+++ wp-content/themes/nananopote/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php wp_title(); ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,900&display=swap" rel="stylesheet">
+
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="header">
+            <div class="wrap">
+                <div class="site">
+                    <h1 class="nomeSite"><a title="<?php bloginfo( 'name' ) ?>" href="<?php site_url(); ?>">NANANOPOTE</a></h1>                    
+                </div>
+            </div>
+        </div>
+        
